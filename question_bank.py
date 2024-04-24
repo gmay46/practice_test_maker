f8b25e90ccae434c19299046c1b273e7613a6945
gmay46 <gmay46@gmail.com>
2024-04-24 12:17:56 -0500

    Add files via upload

---
+++ question_bank.py
@@ -0,0 +1,7 @@
+question_list = [
+["This is a test question 1?", ["correct", "incorrect", "incorrect", "incorrect"], 0],
+["This is a test question 2?", ["correct", "incorrect", "incorrect", "incorrect"], 0],
+["This is a test question 3?", ["incorrect", "correct", "incorrect", "incorrect"], 1],
+["This is a test question 4?", ["incorrect", "incorrect", "correct", "incorrect"], 2],
+["This is a test question 5?", ["incorrect", "incorrect", "incorrect", "correct"], 3]
+]
